8c1a445e27d6141519a6d16c755ba716c4f9f61f
Zohidillo <turgunovzohidillopr@yahoo.com>
2021-08-26 11:43:54 +0500

    SignUp function is working

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, SignUp_function
 
 app_name = 'Users'
 
 urlpatterns = [
-    path('', Home, name='Home')
+    path('', Home, name='Home'),
+    path('signup', SignUp_function, name='SignUp'),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from django.contrib.auth import login
+# from django.contrib import messages
 
 # Create your views here.
 
 
 def Home(request):
-    return HttpResponse('Hello <b>User</b>')
+    return render(request, 'index/UserHome.html')
+
+
+def SignUp_function(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('Users:Home')
+    form = UserForm
+    return render(request, 'register/signUp.html', context={'form': form})
