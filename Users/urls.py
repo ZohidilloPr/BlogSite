1832df007aa4e3e84f3571cc5672056c196f3dc6
Zohidillo <turgunovzohidillopr@yahoo.com>
2021-08-26 14:54:49 +0500

    SignIn, SignOut fuctions are working

--- Users/urls.py
+++ Users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Home, SignUp_function
+from .views import Home, SignUp_function, SignIn_function, SignOut_function
 
 app_name = 'Users'
 
 urlpatterns = [
     path('', Home, name='Home'),
     path('signup', SignUp_function, name='SignUp'),
+    path('signin', SignIn_function, name='SignIn'),
+    path('signout', SignOut_function, name='SignOut'),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
-from django.contrib.auth import login
-# from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -19,3 +20,26 @@ def SignUp_function(request):
             return redirect('Users:Home')
     form = UserForm
     return render(request, 'register/signUp.html', context={'form': form})
+
+
+def SignIn_function(request):
+    if request.method == 'POST':
+        forms = AuthenticationForm(data=request.POST)
+        if forms.is_valid():
+            username = forms.cleaned_data.get('username')
+            password = forms.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('Users:Home')
+            else:
+                return messages.error(request, 'Something went to wrong!')
+        else:
+            return messages.error(request, 'Something went to wrong!')
+    forms = AuthenticationForm
+    return render(request, 'register/signIn.html', context={'login_form': forms})
+
+
+def SignOut_function(request):
+    logout(request)
+    return redirect('Blog:Home')
